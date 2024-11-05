a260720fb3708ac3a2ac6f6d8e044f365da3a13f
Harshit <harshit2001pandey@gmail.com>
2024-11-05 18:16:51 +0530

    Configured post(register)

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "123456",
+  port: 5432,
+});
+
+db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -19,9 +29,19 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(`Username = ${username}, Password = ${password}`);
+  try {
+    await db.query('insert into users (email, password) values ($1, $2)', [username, password]);
+    res.render('secrets.ejs');
+  } catch (error) {
+    console.error("unable to register", error);
+  }
+});
 
-app.post("/login", async (req, res) => {});
+app.post("/login", async (req, res) => { });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
